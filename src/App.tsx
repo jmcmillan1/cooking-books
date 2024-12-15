ffb7e4ccc8adbee6a653a8678f8c9b7c16b1b50f
jmcmillan1 <47255714+jmcmillan1@users.noreply.github.com>
2024-12-14 19:00:12 -0700

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DishoomImage from "./DishoomImage.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a react app where Josh will host his cookbook reviewing blog someday
-        </p>
-        <a
+        Cooking Books: Reviewing recipes from cookbooks
+      </header>
+
+<div  style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div 
+        style={{
+            height: 250, width: 250, borderStyle: 'solid',
+            borderWidth: 2, borderColor: 'navy', marginTop: 15, marginBottom: 5}}>
+              <img height={250} width={250} src={DishoomImage} alt="Image"/>
+      </div>
+      <div style={{display: "flex", flexDirection: "column"}}>
+      <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.dishoom.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dishoom
         </a>
-      </header>
+        <a
+          className="App-link"
+          href="https://store.dishoom.com/products/dishoom-cookery-book"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cookbook link
+        </a>
+        </div>
+        </div>
     </div>
   );
 }
